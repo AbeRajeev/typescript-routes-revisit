5c49045b72bbca2121eb6a77ac1ed8e791a62c57
Abe Rajeev <abhijithgr@hotmail.com>
2023-01-19 17:58:41 -0800

    express server with nodemon watch

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, {Express, Response, Request} from 'express';
+const port = 8000;
+
+const app: Express = express();
+
+app.get('/', (req: Request , res: Response) => {
+    res.send('Hello World! This app says hello to you! in TypeScript!');
+});
+
+app.get('/api', (req: Request , res: Response) => {
+    res.send('Test endpoint');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
